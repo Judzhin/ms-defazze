3b78a66195e118467a3df9c3b7db1345389e6b50
Judzhin Miles <info@msbios.com>
2015-07-27 22:29:36 +0300

    Intermediate commit

---
+++ L2Datatype/L2Datatype/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace L2Datatype
+{
+	/// <summary>
+	/// Main class.
+	/// </summary>
+	class MainClass
+	{
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
